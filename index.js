0f3fa73a61e1633a03289dea10f444a58dcaa1ad
Mercy <maritimmercy8@gmail.com>
2025-03-18 13:15:10 +0300

    move doddger right

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  const moveDodger= document.getElementById('dodger');
+  // function  moveDodgerRight() {
+  //   const left = dodger.style.left.replace
+  //    left= parseInt(left);
+
+  //    moveDodgerRight()
+
+  //    let newPosition = dodger.style.left
+  //    newPosition = parseInt(newPosition)
+     
+  //    if(right<360){
+  //       dodger.style.left= `${left}`;
+  //   }
+  // }
+  
+//   document.addEventListener("keydown"), function (e) {
+//     if (e.key === "Arrow") {
+//       moveDodgerleft();
+//     }
+// }
